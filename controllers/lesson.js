e70d5b0dea0c8943edf26c008faec1a632f07e73
bluexbaez <bluexbaez@gmail.com>
2024-06-24 13:13:17 -0300

    added 404 return for register presence

--- controllers/lesson.js
+++ controllers/lesson.js
@@ -57,58 +57,6 @@ exports.registerPresence = async (req, res, next) => {
 
     return res.json(lessonStudents)
   } else {
-    console.log('Student or Lesson not found')
+    return res.status(404).json({ error: 'Student or Lesson not found' });
   }
 }
-
-// exports.getEditLesson = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.lessonId;
-//   Lesson.findById(prodId, lesson => {
-//     if (!lesson) {
-//       return res.redirect('/');
-//     }
-//     res.render('admin/edit-lesson', {
-//       pageTitle: 'Edit Lesson',
-//       path: '/admin/edit-lesson',in
-//       editing: editMode,
-//       lesson: lesson
-//     });
-//   });
-// };
-
-// exports.postEditLesson = (req, res, next) => {
-//   const prodId = req.body.lessonId;
-//   const updatedTitle = req.body.title;
-//   const updatedPrice = req.body.price;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedDesc = req.body.description;
-//   const updatedLesson = new Lesson(
-//     prodId,
-//     updatedTitle,
-//     updatedImageUrl,
-//     updatedDesc,
-//     updatedPrice
-//   );
-//   updatedLesson.save();
-//   res.redirect('/admin/lessons');
-// };
-
-// exports.getLessons = (req, res, next) => {
-//   Lesson.fetchAll(lessons => {
-//     res.render('admin/lessons', {
-//       prods: lessons,
-//       pageTitle: 'Admin Lessons',
-//       path: '/admin/lessons'
-//     });
-//   });
-// };
-
-// exports.postDeleteLesson = (req, res, next) => {
-//   const prodId = req.body.lessonId;
-//   Lesson.deleteById(prodId);
-//   res.redirect('/admin/lessons');
-// };
